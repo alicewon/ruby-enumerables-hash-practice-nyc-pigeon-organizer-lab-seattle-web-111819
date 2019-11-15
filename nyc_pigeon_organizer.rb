32278c5be18cb38aa67dde5033473224d4909e98
Alice Won <alicewon4@gmail.com>
2019-11-15 22:59:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,29 +22,3 @@ pigeon_list_restructured #got it
 end
 
 
-
-
-
-
-
-
-
-
-
-
-def nyc_pigeon_organizer(data)
-pigeon_list_restructured= {}
-
-data.each do |color_gender_lives, value|
-  value.each do |stats, all_names|
-    all_names.each do |name|
-      if pigeon_list_restructured[name] = nil
-        pigeon_list_restructured[name] = {}
-      end
-      if pigeon_list_restructured[name][color_gender_lives] = nil
-        pigeon_list_restructured[name][color_gender_lives] = []
-end
-  pigeon_list_restructured[name][color_gender_lives].push(stats.to_s)
-end
-pigeon_list_restructured
-end
